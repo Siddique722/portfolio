7cbab9997891e7aca7ac477a811b538a6fc3e7d2
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-11-06 12:07:05 -0800

    update code

--- lib/portfolio_views/container_2/containertwo_responsive/containertwo_desktop.dart
+++ lib/portfolio_views/container_2/containertwo_responsive/containertwo_desktop.dart
@@ -16,22 +16,33 @@ class _ContainertwoDesktopState extends State<ContainertwoDesktop> {
     double height=MediaQuery.of(context).size.height;
     double width=MediaQuery.of(context).size.width;
     return Container(
-      height: height*0.8,
+      //height: height*0.8,
       width: width,
       color: AppColor.lightbgColor,
       child: Column(children: [
+        SizedBox(height: 20,),
         Text('My Services',style: GoogleFonts.inconsolata(
             color: Colors.white,
             fontSize: 42,
             fontWeight: FontWeight.bold
         ),),
+        SizedBox(height: 20,),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
           ColumnWidget(text1: 'Front End', text2: 'I am Nitish Kumar i have Excellent skill of web development and android development.  learner by YouTube. '),
           ColumnWidget(text1: 'Front End', text2: 'I am Nitish Kumar i have Excellent skill of web development and android development.  learner by YouTube. '),
           ColumnWidget(text1: 'Front End', text2: 'I am Nitish Kumar i have Excellent skill of web development and android development.  learner by YouTube. '),
-        ],)
+        ],),
+        SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          ColumnWidget(text1: 'Front End', text2: 'I am Nitish Kumar i have Excellent skill of web development and android development.  learner by YouTube. '),
+          ColumnWidget(text1: 'Front End', text2: 'I am Nitish Kumar i have Excellent skill of web development and android development.  learner by YouTube. '),
+          ColumnWidget(text1: 'Front End', text2: 'I am Nitish Kumar i have Excellent skill of web development and android development.  learner by YouTube. '),
+        ],),
+
 
 
 
@@ -59,11 +70,11 @@ class ColumnWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 180,
-            width: 180,
+            height: 150,
+            width: 150,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(500),
-                border: Border.all(color: AppColor.greenPrimaryColor,width: 9)
+                border: Border.all(color: AppColor.greenPrimaryColor,width: 7)
             ),
             child: Center(child:
             Image.asset('lib/assets/icon.png'),),
