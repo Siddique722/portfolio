b7ab6f288920dd36d2469d79c29b3983de515d32
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-11-06 11:54:31 -0800

    update code

---
+++ lib/portfolio_views/container_2/container-2.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/components/responsive_layout/responsive-layout.dart';
+import 'package:portfolio/portfolio_views/container_2/containertwo_responsive/containertwo_desktop.dart';
+
+
+class ContainerTwoMain extends StatefulWidget {
+  const ContainerTwoMain({super.key});
+
+  @override
+  State<ContainerTwoMain> createState() => _ContainerTwoMainState();
+}
+
+class _ContainerTwoMainState extends State<ContainerTwoMain> {
+  @override
+  Widget build(BuildContext context) {
+    return
+      ResponsiveLayotWidget(
+        desktop: ContainertwoDesktop(),
+        mobile: ContainertwoDesktop(),
+        tablet: ContainertwoDesktop());
+   }
+}
---
+++ lib/portfolio_views/starring/starting-view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/portfolio_views/container_1/container_1.dart';
+import 'package:portfolio/portfolio_views/container_2/container-2.dart';
+
+
+class StartingView extends StatefulWidget {
+  const StartingView({super.key});
+
+  @override
+  State<StartingView> createState() => _StartingViewState();
+}
+
+class _StartingViewState extends State<StartingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          ContainerOneMain(),
+          ContainerTwoMain()
+
+        ],),
+      ),
+    );
+  }
+}
