c1c656fcff552ef4c7356bd06917901c979b59c2
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-11-06 19:58:23 -0800

    update code

--- lib/Controller/constants/color.dart
+++
@@ -1,8 +0,0 @@
-
-import 'dart:ui';
-
-class AppColor{
-  static Color greenPrimaryColor=Color(0xff00DA64);
-  static Color darkbgColor=Color(0xff191A22);
-  static Color lightbgColor=Color(0xff22232E);
-}
